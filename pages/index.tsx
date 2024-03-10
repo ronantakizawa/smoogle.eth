50ae28f4323136ded3e84f9a5022565ce5998541
ronantakizawa <ronantakizawa@gmail.com>
2024-03-10 16:03:13 -0600

    added loading

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
 import { useState, FormEvent } from 'react';
 import { Pinecone } from '@pinecone-database/pinecone';
+import Image from 'next/image';
+import { ClipLoader } from 'react-spinners'; // Step 1: Import ClipLoader
 
 const Home: React.FC = () => {
   const [query, setQuery] = useState<string>('');
   const [results, setResults] = useState<Array<{ Title: string; URL: string, Contract:string, Creator:string,}>>([]);
+  const [loading, setLoading] = useState<boolean>(false); // Step 2: Add a loading state
   const pc = new Pinecone({
     apiKey: process.env.NEXT_PUBLIC_API_KEY ?? "KEY",
   });
@@ -11,11 +14,12 @@ const Home: React.FC = () => {
 
   const handleSearch = async (e: FormEvent) => {
     e.preventDefault();
+    setLoading(true); // Step 3: Toggle loading to true at the start of the search
     try {
       const { pipeline } = await import("@xenova/transformers");
       const extractor = await pipeline("embeddings", "Xenova/all-MiniLM-L6-v2");
-      const embededQuery = await extractor(query, {pooling: 'mean', normalize: true });
-      const data: number[] = Array.from(embededQuery.data);
+      const embeddedQuery = await extractor(query, {pooling: 'mean', normalize: true });
+      const data: number[] = Array.from(embeddedQuery.data);
       const results = await index.query({
         vector: data,
         topK: 5,
@@ -31,13 +35,18 @@ const Home: React.FC = () => {
       })));
     } catch (error) {
       console.error('Search request failed', error);
+    } finally {
+      setLoading(false); // Toggle loading to false when the search is done
     }
   };
 
   return (
     <div className="max-w-4xl mx-auto p-4">
-      <h1 className="text-3xl font-bold text-center text-gray-800">Smoogle.eth</h1>
-      <h4 className="text-2xl mt-4  text-center text-gray-500">A Semantic Smart Contract Search Engine on Ethereum</h4>
+      <div className="flex items-center justify-center">
+        <h1 className="text-3xl font-bold text-center text-gray-800">Smoogle.eth</h1>
+        <Image src="smoogle.eth.svg" width={50} height={50} alt="logo"/> 
+      </div>
+      <h4 className="text-2xl mt-4 text-center text-gray-500">A Semantic Smart Contract Search Engine on Ethereum</h4>
       <form onSubmit={handleSearch} className="mt-4 flex flex-col items-center">
         <input
           type="text"
@@ -51,7 +60,10 @@ const Home: React.FC = () => {
           Search
         </button>
       </form>
-      {results.length > 0 && (
+      {loading && ( 
+        <ClipLoader className="flex justify-center items-center ml-64" color="#007bff" size={350} />
+      )}
+      {!loading && results.length > 0 && (
         <div>
           <h2 className="text-xl font-semibold mt-6 mb-4">Results</h2>
           <ul className="list-none space-y-3 p-2">
